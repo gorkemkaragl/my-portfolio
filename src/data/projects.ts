7132193e1b167d1c967c8ed94ac95bf3f60d75a6
gorkemkaragl <gorkemkaragol0@gmail.com>
2025-12-02 14:13:29 +0300

    Refactor code structure for improved readability and maintainability

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,18 +3,31 @@ export const projects = [
     title: "Kişisel Portfolyo Sitesi",
     description:
       "Modern ve kullanıcı dostu bir e-ticaret web uygulaması. React ve TypeScript ile geliştirildi.",
-    image:
-      "https://images.unsplash.com/photo-1557821552-17105176677c?w=800&h=600&fit=crop",
+    image: "./portfolio.png",
     tags: ["TypeScript", "Next.js", "Tailwind CSS"],
     url: "https://github.com/gorkemkaragl/",
     demoURL: "https://gorkemkaragol.vercel.app",
   },
   {
+    title: "Kur Takip - Döviz Kurları Uygulaması",
+    description:
+      "Kur-Takip, kullanıcıların anlık döviz hesaplaması yapmasını ve döviz kurlarını takip etmesini sağlayan basit bir döviz uygulamasıdır.",
+    image: "./kur-takip.png",
+    tags: [
+      "JavaScript",
+      "Next.js",
+      "Tailwind CSS",
+      "Shadcn UI",
+      "Frankfurter API",
+    ],
+    url: "https://github.com/gorkemkaragl/kur-takip",
+    demoURL: "https://kur-takip.vercel.app",
+  },
+  {
     title: "Ford Otosan Staj Projesi",
     description:
-      "Takımlar için gerçek zamanlı görev takibi ve proje yönetimi platformu.",
-    image:
-      "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?w=800&h=600&fit=crop",
+      "Ford Otosan'da gerçekleştirdiğim staj süresince, Python kullanarak araç verilerini analiz eden ve görselleştiren bir uygulama geliştirdim. Bu proje, veri işleme ve analiz konusundaki becerilerimi geliştirmeme olanak sağladı.",
+    image: "./ford-otosan.png",
     tags: ["Python"],
     url: "https://github.com/gorkemkaragl/FordOtosan_Internship",
     demoURL: "/",
@@ -35,7 +48,7 @@ export const projects = [
       "Serender Nut House, kendimi geliştirmek için React kullanarak sıfırdan geliştirdiğim bir mini e-ticaret uygulamasıdır. Bu proje sayesinde hem front-end mimarisi üzerine pratik yapıyorum hem de gerçek bir e-ticaret akışını adım adım oluşturarak deneyim kazanıyorum.",
     image:
       "https://images.unsplash.com/photo-1499750310107-5fef28a66643?w=800&h=600&fit=crop",
-    tags: ["React", "Next.js", "MDX"],
+    tags: ["React", "Tailwind.css", "Framer Motion"],
     url: "https://github.com/gorkemkaragl/serender-nut-house",
     demoURL: "/",
   },
